ff39c20dd271b78f197277453e5b1da615eb98bb
wcalisaya <elmagwi@gmail.com>
2013-06-25 01:18:37 -0500

    tarea sh

---
+++ tarea.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm -r /home/repository/ingsist
+rm -r /home/repository/projectone
+#mkdir magwi
+
+cd /home/repository; git clone git@github.com:elmagwi/ingsist.git
+cd /home/repository; git clone git@github.com:wcalisaya/projectone.git
+
+
+
+#mkdir /home/repository/maloso
+
+#wget http://logistica.grupolinares.cloudbees.net/twitter-boot/assets/img/logo.png
+
+
+echo descargar complet!
